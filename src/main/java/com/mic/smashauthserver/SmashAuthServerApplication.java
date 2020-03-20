463d0f475cc27b72c2401045a6f3e06d78eaac01
JeanMichou <sechet.marc.27@gmail.com>
2020-03-20 09:41:10 +0100

    initial commit

---
+++ src/main/java/com/mic/smashauthserver/SmashAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.mic.smashauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmashAuthServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SmashAuthServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/mic/smashauthserver/SmashAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mic.smashauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmashAuthServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
